ff398dfeb3ddf1495ae307a70ba73df6810a7a84
SviluppoCodice <51750512+Sviluppocodice@users.noreply.github.com>
2019-07-22 15:12:49 +0200

    Add files via upload

---
+++ Tipi/Tipi.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+print(str)
+
+var a = 1
+var b = 1
+b = 3
+
+print(a - b)
+
+str = "ciao"
+
+print(str)
+
+let c : Int = 3
+
+print(c + a)
+
+var nuovaStringa : String = "Good morning Dev"
+
+
+var d : Float = 2.2
+
+var e : Double = 3.40
+
+a = Int(d)
+
+print(a)
+
+print(round(e))
+
+print(Int(round(e)))
+
+
+
+
+
+
+
+
+
+
+
+
